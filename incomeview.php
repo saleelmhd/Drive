93ce33ba0213d4fc50edde6235d5c596f928b33d
saleelmhd <mohammedsaleeledk@gmail.com>
2024-01-02 15:20:49 +0200

    02/jan

--- incomeview.php
+++ incomeview.php
@@ -1,17 +1,17 @@
 <?php
 include 'connect.php';
-$sql = mysqli_query($con, "SELECT make, model, img, license_plate FROM vehicle_tb WHERE  LEFT(vehicle_type, 1) = '4';
-");
+$sql = mysqli_query($con,"SELECT * FROM `income` ORDER BY `income`.`id` DESC");
 $list = array();
 if ($sql->num_rows > 0) {
     while ($rows = mysqli_fetch_assoc($sql)) {
 
 
         $my_array['result'] = "Success";
-        $my_array['make'] = $rows['make'];
-        $my_array['model'] = $rows['model'];
-        $my_array['img'] = $rows['img'];
-        $my_array['license_plate'] = $rows['license_plate'];
+        $my_array['title'] =$rows['title'];
+        $my_array['pricee'] =$rows['price'];
+       
+        
+
 
         array_push($list, $my_array);
 
